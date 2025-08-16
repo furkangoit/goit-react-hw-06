89c9bfcfeb4671a4e2e1243c348a330da88c963f
furkangoit <celikfurkan45@hotmail.com>
2025-08-16 11:55:34 +0300

    first commit

---
+++ src/components/ContactsBook.jsx
@@ -0,0 +1,60 @@
+import React, { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import {
+    addContact,
+    contactsSlice,
+    deleteContact,
+    loadContacts,
+} from "../redux/constactsSlice";
+import css from "./ContactBook.module.css"
+import { Field, Form, Formik } from "formik";
+import Contact from "./Contact";
+const ContactsBook = () => {
+    const contacts = useSelector((state) => state.contacts.items);
+    const dispatch = useDispatch();
+    const initialValues = {
+        name: " ",
+        number: " ",
+    };
+
+    return (
+        <div>
+            <div className={css.contacts}>
+                <Formik
+                    initialValues={initialValues}
+                    onSubmit={(values, actions) => {
+                        const newContact = {
+                            id: Date.now(),
+                            name: values.name,
+                            phone: values.number,
+                        };
+                        dispatch(addContact(newContact));
+                        actions.resetForm();
+                    }}
+                >
+                    <Form>
+                        <label htmlFor="name">Name</label>
+                        <Field type="text" name="name" id="name"></Field>
+                        <label htmlFor="number">Phone Number</label>
+                        <Field type="text" name="number" id="number"></Field>
+                        <button type="submit" className={css.contactBtn}>Add Contact</button>
+                    </Form>
+                </Formik>
+                <div>
+                    <ul>
+                        {contacts.map((contact) => (
+                            <Contact
+                                key={contact.id}
+                                id={contact.id}
+                                name={contact.name}
+                                phone={contact.phone}
+                            />
+                        ))}
+                    </ul>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default ContactsBook;
---
+++ src/redux/filtersSlide.js
@@ -0,0 +1,18 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+    name: "",
+};
+
+export const filtersSlice = createSlice({
+    initialState,
+    name: "filter",
+    reducers: {
+        filterContact: (state, actions) => {
+            state.name = actions.payload;
+        },
+    },
+});
+
+export const { filterContact } = filtersSlice.actions;
+export default filtersSlice.reducer;
