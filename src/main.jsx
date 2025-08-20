e0bbfd4aa72e569c36bd70d89c18b4bc31555f71
furkangoit <celikfurkan45@hotmail.com>
2025-08-20 19:00:40 +0300

    edited

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { PersistGate } from 'redux-persist/integration/react';
+import { PersistGate } from 'redux-persist/lib/integration/react'
 import { store, persistor } from './redux/store.js';
 import App from './App.jsx';
 import './index.css';
